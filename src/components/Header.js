1b20472be2e0b88b0da9c6315f30983e69803372
Jason Morita <jason.morita@globality.com>
2016-10-02 13:55:05 -0700

    Add some messageFormat content examples

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import defaultMessages from '../../i18n/default.json';
+import { headerStrings } from '../../i18n/default.json';
+
+const state = {
+    name       : 'Eric',
+    unreadCount: 1000,
+    numPhotos: 54
+};
 
 const Header = (props) => {
+
+    const {name, unreadCount, numPhotos} = state;
+
     return (
         <header>
             <div>
-                <FormattedMessage id="headerStrings.title" defaultMessage={defaultMessages.headerStrings.title.defaultMessage} />
+                <FormattedMessage id="headerStrings.title" defaultMessage={headerStrings.title.defaultMessage} />
+            </div>
+            <div>
+                <FormattedMessage id="headerStrings.welcome" defaultMessage={headerStrings.welcome.defaultMessage} values={{name: <b>{name}</b>, unreadCount}} />
+            </div>
+            <div>
+                <FormattedMessage id="headerStrings.numPhotos" defaultMessage={headerStrings.numPhotos.defaultMessage} values={{numPhotos: numPhotos}} />
             </div>
         </header>
     );
