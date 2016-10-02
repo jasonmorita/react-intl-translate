34bb6551ddec0f9ac60885650253401ecdbed9bb
Jason Morita <jason.morita@globality.com>
2016-10-01 18:25:28 -0700

    Add basic translation support

--- src/app.js
+++ src/app.js
@@ -3,30 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
 
-import FormattedInline from './components/formatted-inline';
-import FormattedPredefined from './components/formatted-predefined';
+import Container from './components/Container';
 
-import en from 'react-intl/locale-data/en';
 import es from 'react-intl/locale-data/es';
 import esMessages from '../i18n/es-ES.json';
-import fr from 'react-intl/locale-data/fr';
-import frMessages from '../i18n/fr-FR.json';
 
-const locale = {
-    short: en,
-    name: 'en-US',
-    messages: null
-}
-
-addLocaleData(locale.short);
+addLocaleData([...es]);
 
 const App = (props) => {
   return (
-      <IntlProvider locale={ locale.name } messages={ locale.messages }>
-        <div>
-            <FormattedInline {...props} />
-            <FormattedPredefined {...props} />            
-        </div>
+      <IntlProvider locale="es" messages={ esMessages }>
+        <Container />
       </IntlProvider>
   );
 };
